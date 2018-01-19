e6142eb6fe3be32bc0c00a0d866ed098c73f6a42
oxilod1 <Soyelnumero1?>
2018-01-19 12:35:08 +0200

    0.03

--- src/main/java/UI/Details.java
+++ src/main/java/UI/Details.java
@@ -2,19 +2,39 @@ package UI;
 
 import instanceCreator.InstanceHandler;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class Details implements Initializable {
-    private static List listHandler = InstanceHandler.getRunningInstances();
+    private List listHandler = InstanceHandler.getRunningInstances();
     public ListView instanceList;
+    public Button newInstance;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        instanceList.setItems(FXCollections.observableList(listHandler));
+    }
+
+    public void configLoader(ActionEvent event) throws IOException {
+        Parent configLoader = FXMLLoader.load(getClass().getClassLoader().getResource("config.fxml"));
+        Scene config = new Scene(configLoader);
+        Stage primaryStage = (Stage) newInstance.getScene().getWindow();
+        primaryStage.setScene(config);
+        primaryStage.show();
+    }
+    public void refreshList(ActionEvent event){
+        listHandler = InstanceHandler.getRunningInstances();
         instanceList.setItems(FXCollections.observableList(listHandler));
     }
 }
--- src/main/java/instanceCreator/InstanceHandler.java
+++ src/main/java/instanceCreator/InstanceHandler.java
@@ -3,20 +3,17 @@ package instanceCreator;
 import UI.configController;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.ec2.AmazonEC2AsyncClient;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.*;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.logging.Logger;
 
 
 public class InstanceHandler {
     private static BasicAWSCredentials awsCreds = new BasicAWSCredentials(instanceCreator.Credentials.AWS_ACCESS_KEY_ID, Credentials.AWS_SECRET_ACCESS_KEY);
-    private static final Logger logger = Logger.getLogger("InstanceHandler.class");
-    private static AmazonEC2AsyncClient ec2Client = new AmazonEC2Client(awsCreds).withRegion(Regions.EU_CENTRAL_1);
+    private static AmazonEC2Client ec2Client = new AmazonEC2Client(awsCreds).withRegion(Regions.EU_CENTRAL_1);
 
     public static void instance(){
         configController config = new configController();
@@ -24,7 +21,6 @@ public class InstanceHandler {
         instancesRequest.withImageId(config.selectedImageID).withInstanceType(config.selectedInstanceType).withMinCount(1).withMaxCount(1).withKeyName(config.selectedKeyName).withSecurityGroups(config.selectedGroupName);
         ec2Client.runInstances(instancesRequest);
         System.out.println("Instance is running");
-        logger.info("Hello World");
     }
 
     public static List getRunningInstances() {
